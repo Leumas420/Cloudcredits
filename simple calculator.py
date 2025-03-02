93a59d2c6df735713c54f9d1652b618a044e874a
Samuel Murmu <samuelsam6078@gmail.com>
2025-03-02 22:24:17 +0530

    simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -13,29 +13,29 @@ def multiplication (num1, num2):
 def division (num1, num2):
     result = round((num1 / num2),2)
     print("Division : ", result)
+    
+while True:
+    
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * ")
+    print("---------------- SIMPLE CALCULATOR ---------------")
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * ")
 
+    print("1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n5. Quit")
+    choice = int(input("Which operation do you want to perform : "))
 
-print("* * * * * * * * * * * * * * * * * * * * * * * * * ")
-print("---------------- SIMPLE CALCULATOR ---------------")
-print("* * * * * * * * * * * * * * * * * * * * * * * * * ")
+    a = int(input("Enter first number "))
+    b = int(input("Enter second number "))
 
-a = int(input("Enter first number "))
-b = int(input("Enter second number "))
+    if choice == 1:
+        addition(a, b)
+    elif choice == 2:
+        subtraction(a, b)
+    elif choice == 3:
+        multiplication(a, b)
+    elif choice == 4:
+        division(a, b)
+    elif choice == 5:
+        break
 
-print("1. Addition \n2. Subtration \n3. Multiplication \n4. Division")
-choice = int(input("Which operation do you want to perform : "))
-
-if choice == 1:
-    addition(a, b)
-elif choice == 2:
-    subtraction(a, b)
-elif choice == 3:
-    multiplication(a, b)
-elif choice == 4:
-    division(a, b)
-else:
-    print("Incorrect input")
-
-
-print("* * * * * * * * * * * * * * * * * * * * * * * * * ")
+    print("* * * * * * * * * * * * * * * * * * * * * * * * * \n\n")
 
